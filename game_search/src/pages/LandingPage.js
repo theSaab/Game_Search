4b5878f1e702321783772388ee54e66e5c777994
Saba Fathi <saba.fathi@emailgmail.info>
2020-06-15 12:35:52 -0400

    progress in search bar

--- game_search/src/pages/LandingPage.js
+++ game_search/src/pages/LandingPage.js
@@ -5,14 +5,15 @@ import React, { Component } from 'react'
 export class LandingPage extends Component {
     render() {
         return (
-            <div className='wrapper'>
-                <input type='text' 
-                className='input' 
-                placeholder='What game are you looking for?'/>
-            <div className='search-button'>
-                <i className='fa fa-search' aria-hidden='true' />
-            </div>
-
+            <div className='background'>
+                <div className='wrapper'>
+                    <input type='text'
+                        className='input'
+                        placeholder='What game are you looking for?' />
+                    <div className='search-button'>
+                        <i className='fa fa-search' aria-hidden='true' />
+                    </div>
+                </div>
             </div>
         )
     }
